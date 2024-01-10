bb1fd8910c8018c92440ccdcebc488cf3f47576c
jorgechavez99 <jorgeluischavez.jlcm@gmail.com>
2024-01-10 01:12:15 +0100

    develop: promesas completadas falta añadir regExp

--- js/main.js
+++ js/main.js
@@ -4,31 +4,52 @@ const comprobar = document.querySelector("#comprobar");
 const tablaResultado = document.querySelector("#tablaResultado");
 const formulario = document.querySelector("#formulario")
 const parrafo = document.querySelector("#parrafo")
+
+
+
 //genero los "Arrays" que en este caso viene a ser la base de datos.
 
 const arrayCoches = [
     {
         matricula: "123-b",
         modelo: "Nissan",
-        propiedad: "Jose",
+        propietario: "Jose",
 
     },
     {
         matricula: "234-c",
         modelo: "Suzuki",
-        propiedad: "Carmen",
+        propietario: "Carmen",
 
     },
     {
         matricula: "456-d",
         modelo: "Toyota",
-        propiedad: "Juan",
+        propietario: "Juan",
 
     },
     {
         matricula: "567-f",
         modelo: "Suzuki",
-        propiedad: "Camila",
+        propietario: "Camila",
+
+    },
+    {
+        matricula: "777-a",
+        modelo: "Honda",
+        propietario: "Mary",
+
+    },
+    {
+        matricula: "888-b",
+        modelo: "Mercedez Benz",
+        propietario: "Carlos",
+
+    },
+     {
+        matricula: "111-a",
+        modelo: "Toyota",
+        propietario: "Pablo",
 
     },
 ];
@@ -36,88 +57,107 @@ const arrayCoches = [
 const arrayMultas = [
     {
         multa: ["velocidad"],
-        matricula: "567-f"
+        matricula: "567-f",
     },
     {
         multa: ["estacionamiento", "velocidad"],
-        matricula: "456-d"
+        matricula: "456-d",
     },
     {
         multa: ["estacionamiento"],
-        matricula: "123-b"
-    }
+        matricula: "123-b",
+    },
+    {
+        multa: ["Atropello"],
+        matricula: "888-b",
+    },
 ];
 
 
+let matricula;
+let multa;
 
 //Genero las funciones y eventos
 
 formulario.addEventListener('submit', (ev) => {
-ev.preventDefault()
-    const input=introMatricula.value
+    ev.preventDefault()
+    const input = introMatricula.value
     comprobarMatricula(input)
-
+    pintarParrafo()
+    limpiarTabla()
 })
 
-const comprobarMatricula = (input) => {
-// aca hare la validacion con RegeX y me mostrara si es todo correcto
 
-
-const reciboMatricula = input
-
-    buscarMatricula(reciboMatricula)
-    .then((resp) => {
-     console.log(`la matricula ${resp} si esta en sistema `)
-     })
-     .catch((error) => console.log(error))
-
-    
-}
-
+//En esta funcion trabajamos con promesas en la cual interactuamos entre 2 bases de datos arrayMultas y arrayCoches
 const buscarMatricula = (reciboMatricula) => {
 
     const verMatricula = arrayCoches.find((item) => item.matricula == reciboMatricula)?.matricula
+    console.log(verMatricula)
     return new Promise((resolve, reject) => {
         if (verMatricula) {
-            resolve(verMatricula)
-
+            resolve(verMatricula);
         }
         else {
-            reject(`La matricula ${reciboMatricula} no esta en sistema`)
-
+            reject(`el coche con matricula ${reciboMatricula} no existe`)
         }
+    })
+}
 
+const buscarMultas = (reciboMatricula) => {
+    //console.log("si entra",reciboMatricula)
+    const verMulta = arrayMultas.find((item) => item.matricula == reciboMatricula)?.multa
+    //console.log("dentro de multa", verMulta)
+    return new Promise((resolve, reject) => {
+        if (verMulta) { resolve(verMulta) }
+        else { reject(`el coche con matricula ${reciboMatricula} no tiene multas`) }
     })
 
 }
 
-const buscarMultas = (reciboMatricula) => {
 
+const comprobarMatricula = (input) => {
+    // aca hare la validacion con RegeX
 
-}
-
-
-
-const pintarParrafo = () => {
-
-
-
-}
-
-
-
-const validarMultas = () => {
-
-
+    const reciboMatricula = input
+    
+    buscarMatricula(reciboMatricula)
+        .then((respuesta) => {
+            matricula = respuesta
+            return buscarMultas(reciboMatricula)
+        }).then((respuesta) => {
+            multa = respuesta
+            pintarTabla(matricula,multa)
+            console.log(`El coche con matricula ${matricula} tiene multas por ${multa}`)
+        })
+        .catch((error) => pintarParrafo(error))
 
 }
 
+// Esta funcion permite añadir un parrafo informado las diferentes situaciones que se presentan
+const pintarParrafo = (error) => {
+parrafo.innerHTML='';
+    parrafo.textContent=error
+}
+
+// Esta funcion permite añadir los elementos extraidos dentro de una tabla
+const pintarTabla = (matricula,multa) => {
+
+    const recogeElementos = arrayCoches.find((item) => item.matricula == matricula)
+
+    const {modelo,propietario}=recogeElementos
+ 
+    tablaResultado.innerHTML=`<td>${matricula}</td>
+                                <td>${modelo}</td>
+                             <td>${propietario}</td>
+                             <td>${multa}</td>`
+    
+}
+
+const limpiarTabla=()=>{
+    tablaResultado.innerHTML=''
+}
 
 
-
-const pintarTabla = () => { }
-
-comprobarMatricula()
 
 
 
